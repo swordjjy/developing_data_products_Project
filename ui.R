13cdeaa65b0614b56844dde3b42421838318c64f
Jiayi Jiang <jiayi.jiang@vanderbilt.edu>
2015-07-23 01:36:01 +0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,12 +1,4 @@
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-# 
-# http://www.rstudio.com/shiny/
-#
-
 library(shiny)
-
-# Fix tag("div", list(...)) : could not find function "showOut… 
 library(rCharts)
 
 shinyUI(
